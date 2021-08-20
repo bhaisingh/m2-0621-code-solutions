c1aa287420756f066512baa6583358e3a5d4c3a9
Rakesh Singh <bhaisingh@gmail.com>
2021-08-19 18:12:18 -0700

    Completed Post and delete middleware.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const fs = require('fs');
 const inputFileName = 'data.json'
 
+// Clients can GET a list of notes.
+
 app.get('/api/notes', (req, res) => {
 
     fs.readFile(inputFileName, 'utf8', (err, data) => {
@@ -24,6 +26,10 @@ app.get('/api/notes', (req, res) => {
         } 
     });
 })
+
+
+// Clients can GET a single note by id.
+
 
 app.get('/api/notes/:id', (req, res) => {
     if (parseInt(req.params.id) <= 0 &&  typeof req.params.id !== 'number' && req.params.id % 1 !== 0) {
@@ -58,6 +64,91 @@ app.get('/api/notes/:id', (req, res) => {
         })
     }
 });
+
+// Clients can POST a new note.
+
+app.use(express.json())
+
+app.post('/api/notes', (req, res) => {
+    if (req.body.content === undefined) {
+        res.status(400);
+        res.json({"error": "content is a required field"})
+    } else {
+        fs.readFile(inputFileName, 'utf8', (err, data) => {
+            if (err) {
+                console.log("Error reading File:",  err);
+            } try {
+                const content = JSON.parse(data);
+                const writeObj = {
+                    "content": req.body.content,
+                    "id": content.nextId
+                }
+                let nextId = content.nextId
+                content.notes[nextId] = writeObj;
+                content.nextId++;
+                data = JSON.stringify(content);
+                fs.writeFile(inputFileName, data, (err) => {
+                    if (err) {
+                        res.status(500);
+                        res.json({
+                            "error": "An unexpected error occurred."
+                        })
+                    } else {
+                        res.status(201);
+                        res.json(writeObj);
+                    }
+                })
+            } catch (err) {
+                console.log("Error parsing JSON string: ", err);
+            } 
+        })
+    }
+})
+
+
+// Clients can DELETE a note by id.
+
+app.delete('/api/notes/:id', (req, res) => {
+    if (parseInt(req.params.id) <= 0 &&  typeof req.params.id !== 'number' && req.params.id % 1 !== 0) {
+        res.status(400);
+        res.json({"error": "id must be a positive number"})
+    } else {
+        fs.readFile(inputFileName, 'utf8', (err, data) => {
+            if (err) {
+                console.log("Error reading File:",  err);
+            } try {
+                const content = JSON.parse(data);
+                const notesValue ;
+                for (const outObj in content.notes) {
+                    if (content.notes[outObj].id === parseInt(req.params.id)) {
+                        notesValue = content.notes[outObj];
+                        delete content.notes[outObj];
+                    }
+                }
+                content = JSON.stringify(content);
+                fs.writeFile(inputFileName, data, (err) => {
+                    if (err) {
+                        res.status(500);
+                        res.json({
+                            "error": "An unexpected error occurred."
+                        })
+                    } else if (notesValue === undefined) {
+                        res.status(404);
+                        res.json({
+                            "error": `note with id ${req.params.id} does not exist`
+                        })
+                    } else {
+                        res.sendStatus(204);
+                    }
+                }
+            } catch (err) {
+                console.log("Error parsing JSON string: ", err);
+            } 
+            
+        })
+    }
+})
+
 
 
 app.listen(3000, () => {
